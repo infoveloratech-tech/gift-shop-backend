37937440b9bb3d7cebd186c4e282dcb17d847a01
velora technology <info.veloratech@gmail.com>
2026-07-05 10:28:03 +0530

    swagger

--- gift-shop/Program.cs
+++ gift-shop/Program.cs
@@ -91,7 +91,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gift Shop API V1");
+    c.RoutePrefix = string.Empty;
+});
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
 app.UseMiddleware<ExceptionMiddleware>();
